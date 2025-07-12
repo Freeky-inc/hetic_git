ea08bee894f82d90c389b87a200e6a48175ecab5
bnjjs1998 <b_jaques@hetic.eu>
2025-07-12 17:13:52 +0200

    début de l'issus rm files

--- functions/rm_file.py
+++ functions/rm_file.py
@@ -2,5 +2,9 @@ import os
 import json
 import hashlib
 
-def remove_file():
-    print('une commande pour que le fichier soit suprimé')
+def remove_file(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print(f"Fichier '{file_path}' supprimé.")
+    else:
+        print(f"Fichier '{file_path}' introuvable.")
