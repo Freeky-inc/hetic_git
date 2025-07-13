a484ab4239f3fd6939f3e8b292b9e4fe9bd91ce3
bnjjs1998 <b_jaques@hetic.eu>
2025-07-13 18:45:37 +0200

    ajout de remove from index

--- functions/rm_file.py
+++ functions/rm_file.py
@@ -2,9 +2,24 @@ import os
 import json
 import hashlib
 
-def remove_file(file_path):
+def remove_from_index(file_path):
+    index_path = ".fyt/index"
+
+    if not os.path.exists(index_path):
+        return
+    with open(index_path, "r") as f:
+        index = json.load(f)
+    rel_path = os.path.relpath(file_path, os.getcwd())
+    if rel_path in index:
+        del index[rel_path]  # Supprimer le fichier de l'index
+
+        with open(index_path, "w") as f:
+            json.dump(index, f)
+
+def fyt_rm(file_path):
     if os.path.exists(file_path):
         os.remove(file_path)
-        print(f"Fichier '{file_path}' supprimé.")
+        remove_from_index(file_path)
+        print(f"Removed '{file_path}' from working directory and index.")
     else:
-        print(f"Fichier '{file_path}' introuvable.")
+        print(f"File '{file_path}' does not exist.")
